5da30119e1da6c553161a135c60b1fdcf897bfeb
uiyer <uiyer@CMW-RDS04>
2021-03-23 07:46:28 -0400

    Module 2

--- src/app.js
+++ src/app.js
@@ -7,8 +7,29 @@ app.use(express.static(__dirname + "/public"));
 app.set("views", path.join(__dirname, "/views"));
 app.set("view engine", "ejs");
 
+var accountData = fs.readFileSync("src/json/accounts.json", {
+  encoding: "UTF8"
+});
+const accounts = JSON.parse(accountData);
+var userData = fs.readFileSync("src/json/users.json", {
+  encoding: "UTF8"
+});
+const users = JSON.parse(userData);
+
 app.get("/", function(req, res) {
-  res.render("index", { title: "Index" });
+  res.render("index", { title: "Account Summary", accounts });
+});
+app.get("/savings", function(req, res) {
+  res.render("account", { account: accounts.savings });
+});
+app.get("/checking", function(req, res) {
+  res.render("account", { account: accounts.checking });
+});
+app.get("/credit", function(req, res) {
+  res.render("account", { account: accounts.credit });
+});
+app.get("/profile", function(req, res) {
+  res.render("profile", { user: users[0] });
 });
 app.listen(3000, function() {
   console.log("PS Project Running on port 3000!");
