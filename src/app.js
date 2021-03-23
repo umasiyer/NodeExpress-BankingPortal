ae50cb4a3d4d5e0fed72561e6858bc12c557c460
uiyer <uiyer@CMW-RDS04>
2021-03-23 05:38:36 -0400

    module 1

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const path = require("path");
+const express = require("express");
+
+const app = express();
+app.use(express.static(__dirname + "/public"));
+app.set("views", path.join(__dirname, "/views"));
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res) {
+  res.render("index", { title: "Index" });
+});
+app.listen(3000, function() {
+  console.log("PS Project Running on port 3000!");
+});
